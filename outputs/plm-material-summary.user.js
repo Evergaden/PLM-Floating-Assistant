0b824c1fd0ebcb4b1bf6a7e46c94b414fd4b19c3
Violet <violet@example.local>
2026-07-27 21:17:22 +0800

    set script version to 2.6.1

--- outputs/plm-material-summary.user.js
+++ outputs/plm-material-summary.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         PLM悬浮助手
 // @namespace    https://plm.westmonth.com/
-// @version      2.9
+// @version      2.6.1
 // @description  Store PLM project packaging specs locally and show them in a floating helper.
 // @author       Violet
 // @match        https://plm.westmonth.com/*
@@ -32,7 +32,7 @@
 
   const PANEL_ID = 'plm-floating-helper';
   const LAUNCHER_ID = 'plm-floating-helper-launcher';
-  const SCRIPT_VERSION = '2.9';
+  const SCRIPT_VERSION = '2.6.1';
   const REVIEW_CONFIRM_WAIT_MS = 30000;
   const UPLOAD_PAGE_IDLE_TIMEOUT_MS = 12000;
   const UPLOAD_PAGE_IDLE_STABLE_MS = 1200;
