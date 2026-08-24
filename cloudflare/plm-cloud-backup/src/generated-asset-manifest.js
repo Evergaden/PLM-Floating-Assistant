1ee1463c32acc9e8cf8dab024cab11b6ca234914
Violet <violet@example.local>
2026-08-24 09:37:41 +0800

    fix(ui): lock detail tabs to one fixed track

--- cloudflare/plm-cloud-backup/src/generated-asset-manifest.js
+++ cloudflare/plm-cloud-backup/src/generated-asset-manifest.js
@@ -20,9 +20,9 @@ export const STATIC_ASSET_MANIFEST = {
       "sha256": "c612ee5b7df3d79bb6c15ffea30c5748c4a9dd9cd2086d3384bac8eea456eb50"
     },
     "uiStyles": {
-      "path": "v15/ui-2.5.253.css",
-      "bytes": 689347,
-      "sha256": "e4f05658b81004383e6c8f212d61f1784535827fce01b0bb52021296f6d6096e"
+      "path": "v15/ui-2.5.254.css",
+      "bytes": 689583,
+      "sha256": "168cecd809998d78a174759821278286faf6b901e0ac9b70e0b36e0e2d560aa9"
     },
     "uiStyleVersions": {
       "2.5.108": {
@@ -195,10 +195,10 @@ export const STATIC_ASSET_MANIFEST = {
         "bytes": 654608,
         "sha256": "81019b78e62aea3f5d51fb8f1596e85946b678105cb554dd87e18ce04406669b"
       },
-      "2.5.253": {
-        "path": "v15/ui-2.5.253.css",
-        "bytes": 689347,
-        "sha256": "e4f05658b81004383e6c8f212d61f1784535827fce01b0bb52021296f6d6096e"
+      "2.5.254": {
+        "path": "v15/ui-2.5.254.css",
+        "bytes": 689583,
+        "sha256": "168cecd809998d78a174759821278286faf6b901e0ac9b70e0b36e0e2d560aa9"
       }
     }
   }
